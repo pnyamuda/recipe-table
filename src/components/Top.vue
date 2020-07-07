79c178f9d18015d371af0e117cc19ee6b9ac4f3c
Pierce Nyamuda <ptnrlab@gmail.com>
2020-07-07 02:39:20 +0200

    search button on nav bar added

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,35 +1,41 @@
 <template>
-	<div id="my-block">
+    <div id="my-block">
 
-		<div id="nav-container">
-			<mdb-navbar color="default-color-dark" dark>
-				<mdb-navbar-brand href="https://mdbootstrap.com/">
-					Recipe Table
-				</mdb-navbar-brand>
-				<mdb-navbar-toggler>
-					<mdb-navbar-nav>
-						<mdb-nav-item href="#" active><router-link to="/">Home</router-link></mdb-nav-item>
-						<mdb-nav-item href="#">Recipe</mdb-nav-item>
-						<mdb-nav-item href="#"><router-link to="/search">Advanced Search</router-link></mdb-nav-item>
-						<mdb-dropdown tag="li" class="nav-item">
-							<mdb-dropdown-toggle tag="a" navLink color="default-color-dark" slot="toggle" waves-fixed>Dropdown</mdb-dropdown-toggle>
-							<mdb-dropdown-menu>
-								<mdb-dropdown-item>Action</mdb-dropdown-item>
-								<mdb-dropdown-item>Another action</mdb-dropdown-item>
-								<mdb-dropdown-item>Something else here</mdb-dropdown-item>
-							</mdb-dropdown-menu>
-						</mdb-dropdown>
-					</mdb-navbar-nav>
-					<form>
-						<mdb-input type="text" class="text-white" placeholder="Search" aria-label="Search" label navInput waves waves-fixed />
-					</form>
-				</mdb-navbar-toggler>
-			</mdb-navbar>
+        <div id="nav-container">
+            <mdb-navbar color="default-color-dark" dark>
+                <mdb-navbar-brand href="https://mdbootstrap.com/">
+                    Recipe Table
+                </mdb-navbar-brand>
+                <mdb-navbar-toggler>
+                    <mdb-navbar-nav>
+                        <mdb-nav-item href="#" active>
+                            <router-link to="/">Home</router-link>
+                        </mdb-nav-item>
+                        <mdb-nav-item href="#">Recipe</mdb-nav-item>
+                        <mdb-nav-item href="#">
+                            <router-link to="/search">Advanced Search</router-link>
+                        </mdb-nav-item>
+                        <mdb-dropdown tag="li" class="nav-item">
+                            <mdb-dropdown-toggle tag="a" navLink color="default-color-dark" slot="toggle" waves-fixed>Dropdown</mdb-dropdown-toggle>
+                            <mdb-dropdown-menu>
+                                <mdb-dropdown-item>Action</mdb-dropdown-item>
+                                <mdb-dropdown-item>Another action</mdb-dropdown-item>
+                                <mdb-dropdown-item>Something else here</mdb-dropdown-item>
+                            </mdb-dropdown-menu>
+                        </mdb-dropdown>
+                    </mdb-navbar-nav>
+                    <mdb-form-inline color="default-color-dark">
+                        <mdb-input v-model="quickSearch" class="mr-sm-2" type="text" placeholder="over 1000 recipes" aria-label="Search" />
+                        <mdb-btn @click="makeQuickSearch" outline="white" size="sm" class="my-0" type="submit">Search</mdb-btn>
+                    </mdb-form-inline>
+                </mdb-navbar-toggler>
+            </mdb-navbar>
 
 
-		</div>
+        </div>
 
-	</div>
+
+    </div>
 </template>
 
 
@@ -38,42 +44,54 @@
 
 
 <script>
-	import {
-		mdbNavbar,
-		mdbNavbarBrand,
-		mdbNavbarToggler,
-		mdbNavbarNav,
-		mdbNavItem,
-		mdbDropdown,
-		mdbDropdownMenu,
-		mdbDropdownToggle,
-		mdbInput,
-		mdbDropdownItem
-	} from 'mdbvue';
+    import {
+        mdbNavbar,
+        mdbNavbarBrand,
+        mdbNavbarToggler,
+        mdbNavbarNav,
+        mdbNavItem,
+        mdbDropdown,
+        mdbDropdownMenu,
+        mdbDropdownToggle,
+        mdbInput,
+        mdbDropdownItem,
+        mdbFormInline,
+        mdbBtn,
+    } from 'mdbvue';
 
 
-	export default {
-		name: 'NavbarPage',
-		components: {
-			mdbNavbar,
-			mdbNavbarBrand,
-			mdbNavbarToggler,
-			mdbNavbarNav,
-			mdbNavItem,
-			mdbDropdown,
-			mdbDropdownMenu,
-			mdbDropdownToggle,
-			mdbDropdownItem,
-			mdbInput,
-		}
-	}
+    export default {
+        name: 'NavbarPage',
+        components: {
+            mdbNavbar,
+            mdbNavbarBrand,
+            mdbNavbarToggler,
+            mdbNavbarNav,
+            mdbNavItem,
+            mdbDropdown,
+            mdbDropdownMenu,
+            mdbDropdownToggle,
+            mdbDropdownItem,
+            mdbInput,
+            mdbFormInline,
+            mdbBtn,
+        },
+        data() {
+            return {
+                quickSearch: "",
+            }
+        },
+        methods: {
+            makeQuickSearch() {
+                this.$router.push(`/search/query=${this.quickSearch}`)
+
+            },
+        }
+    }
 
 </script>
 
 <style>
-
-	#nav-container {
-		
-	}
+    #nav-container {}
 
 </style>
